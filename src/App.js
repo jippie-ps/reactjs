5105868e8ff623bf0ae42c3dc3659f24c40401cc
jippie-ps <60638157+jippie-ps@users.noreply.github.com>
2020-02-07 13:00:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React on Openshift</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload - edited by Wendy Chong.
+          To get started, edit <code>src/App.js</code> and save to reload - edited.
       This is prepared for Pipeline Demos
+      ITGroup Demos
         </p>
       </div>
     );
