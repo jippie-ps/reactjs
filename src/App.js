825079b8b9dc7b3282cc8f63a892f3ed00f0d0de
jippie-ps <60638157+jippie-ps@users.noreply.github.com>
2020-02-07 13:02:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload - edited.
       This is prepared for Pipeline Demos
       ITGroup Demos
+      Ver. 1.0.001
         </p>
       </div>
     );
